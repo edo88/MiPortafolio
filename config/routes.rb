7a8eedaafa9071ef4ca3f011a38210d94415a526
edu <valenzuela.edo@gmail.com>
2023-05-17 11:48:06 -0400

    first commit bootstrap card e images

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get '/', to: 'pages#home'
+  get '/home', to: 'pages#home'
+  get '/projects', to: 'pages#projects'
+  get '/contact', to: 'pages#contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
